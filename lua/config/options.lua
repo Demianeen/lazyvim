0945c181a6420b78f5802caed809273faed18da4
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-12-14 07:50:40 +0000

    Fix comment behaviour

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,11 +6,15 @@ vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
 vim.opt.smartindent = true -- Tries to guess and adds intend when start a new line
 vim.opt.autoindent = true -- Remembers indent from previous lines
 
+vim.opt.textwidth = 80 -- text width. Used for example for comments max length
+
 vim.opt.undofile = true -- Sets persistent undo
 
 -- vim.opt.hlsearch = false -- no highlight during search
 
 vim.opt.scrolloff = 8 -- Always would be at least n characters in the bottom/ at the top of screen unless the end of the file
-vim.opt.signcolumn = "yes" -- Always show columns for sign at the left part of the window
+vim.opt.signcolumn = 'yes' -- Always show columns for sign at the left part of the window
 
 vim.opt.report = 1000
+
+vim.opt.spell = true -- enables spelling
