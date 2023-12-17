71d404732335ef6b19ebca73816153c4540967fd
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-12-17 06:44:36 +0000

    Basic nvim setup (#1)
    * Move mason file to lsp folder
    
    * Add rgp snippet
    
    * Add dial nvim custom dials, customized per lang
    
    * Fix comment behaviour
    
    * Remove jk keymap
    
    * Add persistent breakpoints
    
    * Add nvim lua dap
    
    * Update harpoon to harpoon2
    
    * Swap vim-surround with mini.surround, flash on S
    
    * Correct spelling, update vim dictionary, lazy.lock
    
    * Added neo-tree grep in folder command
    
    * Add Comment.nvim
    
    * Swap back to mini.comment
    
    * Add symbol usage plugin
    
    * Remove neotest & dap folder, move symbol usage to lsp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- resetting options
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions:remove('r')
+    vim.opt_local.formatoptions:remove('o')
+    vim.opt_local.formatoptions:append('/')
+    vim.opt_local.formatoptions:append('1')
+  end,
+})
