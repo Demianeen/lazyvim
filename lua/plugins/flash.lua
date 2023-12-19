a64c7c68cab9c8e675f2c5d707a28251a56d6ae0
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-12-19 06:17:30 +0000

    Delete flash and noice configurations to use lazy ones

--- lua/plugins/flash.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'folke/flash.nvim',
-  -- stylua: ignore
-  keys = {
-    { "S", mode = { "n", "x", "o" }, false},
-    { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  },
-}
