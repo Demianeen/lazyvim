32aa4a895582e608f26db790cecf445f9709bafa
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-12-19 06:18:46 +0000

    Add nvim colorizer

---
+++ lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,12 @@
+return {
+  'norcalli/nvim-colorizer.lua',
+  opts = {
+    'css',
+    'scss',
+    'sass',
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+  },
+}
